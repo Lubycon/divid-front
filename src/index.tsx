e783fb2a9057df21d32f18127866de344a1898ef
mia <mia.lee1@kakaocommerce.com>
2021-04-25 18:59:45 +0900

    feat: initial setting

---
+++ src/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { useLocation } from 'react-router';
+
+import Landing from './pages/landing';
+import Login from './pages/login';
+
+interface RouteProps {
+  path: string;
+  component: () => JSX.Element;
+}
+
+const routes: RouteProps[] = [
+  {
+    path: '/landing',
+    component: Landing
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+];
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <Switch location={location}>
+      <Route exact path="/" render={() => <Redirect to="landing" />} />
+      {routes.map((route, i) => (
+        <Route key={i} path={route.path} component={route.component} exact />
+      ))}
+    </Switch>
+  );
+}
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
