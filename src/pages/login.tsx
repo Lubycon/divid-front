eaf872c7287b383a9e2cab07818a17df1854c7e2
mia <mia.lee1@kakaocommerce.com>
2021-04-25 21:24:10 +0900

    add: emotion library

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { css } from '@emotion/react';
+
+const sample = css`
+  color: hotpink;
+  font-size: 20px;
+`;
 
 export default function Login() {
-  return <div>login</div>;
+  return <div css={sample}>login</div>;
 }
