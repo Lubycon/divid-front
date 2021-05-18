f38af8a39f662b44cf1c47a475d980c8c493ec2a
MiaJLee <angella555@naver.com>
2021-05-18 09:04:01 +0900

    fix: 더미 데이터 프로필 Animals enum으로 수정

--- src/pages/trip/log-list.tsx
+++ src/pages/trip/log-list.tsx
@@ -8,22 +8,22 @@ import Log from './log';
 interface Dummy {
   id: number;
   expender: string;
-  profile: keyof typeof Animals;
+  profile: Animals;
   amount: number;
   desc: string;
   date: string;
 }
 
 const DUMMY: Dummy[] = [
-  { id: 1, expender: '지형', profile: 'Puppy', amount: 20000, desc: '휴게소', date: '2021-04-16' },
-  { id: 2, expender: '주예', profile: 'Hamster', amount: 8000, desc: '아메리카노', date: '2021-04-16' },
-  { id: 3, expender: '유진', profile: 'Rabbit', amount: 180000, desc: '게스트하우스', date: '2021-04-16' },
-  { id: 4, expender: '유진', profile: 'Rabbit', amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
-  { id: 5, expender: '유진', profile: 'Rabbit', amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
-  { id: 6, expender: '유진', profile: 'Unicorn', amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
-  { id: 7, expender: '유진', profile: 'Rabbit', amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
-  { id: 8, expender: '유진', profile: 'Bear', amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
-  { id: 9, expender: '유진', profile: 'Rabbit', amount: 180000, desc: '게스트하우스', date: '2021-04-15' }
+  { id: 1, expender: '지형', profile: Animals.Puppy, amount: 20000, desc: '휴게소', date: '2021-04-16' },
+  { id: 2, expender: '주예', profile: Animals.Hamster, amount: 8000, desc: '아메리카노', date: '2021-04-16' },
+  { id: 3, expender: '유진', profile: Animals.Rabbit, amount: 180000, desc: '게스트하우스', date: '2021-04-16' },
+  { id: 4, expender: '유진', profile: Animals.Rabbit, amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
+  { id: 5, expender: '유진', profile: Animals.Rabbit, amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
+  { id: 6, expender: '유진', profile: Animals.Unicorn, amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
+  { id: 7, expender: '유진', profile: Animals.Rabbit, amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
+  { id: 8, expender: '유진', profile: Animals.Bear, amount: 180000, desc: '게스트하우스', date: '2021-04-15' },
+  { id: 9, expender: '유진', profile: Animals.Rabbit, amount: 180000, desc: '게스트하우스', date: '2021-04-15' }
 ];
 
 const Wrap = styled.div`
