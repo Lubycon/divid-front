7497b06148f4831dde52869160a0740f0dbe3aa8
MiaJLee <angella555@naver.com>
2021-06-03 20:23:25 +0900

    fix: http 호출 훅 관심사에 맞게 파일 분리

---
+++ src/hooks/data/useExpense.ts
@@ -0,0 +1,39 @@
+import http from 'api';
+import { ExpenseInfo } from 'model/expense'
+import { useQuery } from 'react-query';
+
+interface Response {
+  message: string;
+}
+
+function getExpenseInfo(tripId: string, expenseId: number) {
+  return http.get<ExpenseInfo>(`/expenses?tripId=${tripId}&expenseId=${expenseId}`);
+}
+
+function editExpenseInfo(tripId: string, expenseId: number) {
+  return http.put<Response, ExpenseInfo>(`/expenses?tripId=${tripId}&expenseId=${expenseId}`);
+}
+
+function postExpense(data: ExpenseInfo) {
+  return http.post<Response, ExpenseInfo>('/expenses', data);
+}
+
+function deleteExpense(tripId: string, expenseId: number) {
+  return http.post<Response, ExpenseInfo>(`/expenses?tripId=${tripId}&expenseId=${expenseId}`);
+}
+
+export function useGetExpenseInfo(tripId: string, expenseId: number) {
+  return useQuery('getExpenseInfo', () => getExpenseInfo(tripId, expenseId));
+}
+
+export function useEditExpenseInfo(tripId: string, expenseId: number) {
+  return useQuery('getExpenseInfo', () => editExpenseInfo(tripId, expenseId));
+}
+
+export function usePostExpense(data: ExpenseInfo) {
+  return useQuery('postExpense', () => postExpense(data));
+}
+
+export function useDeleteExpense(tripId: string, expenseId: number) {
+  return useQuery('deleteExpense', () => deleteExpense(tripId, expenseId));
+}
---
+++ src/hooks/data/useMyPage.ts
@@ -0,0 +1,32 @@
+import http from 'api';
+import { MyInfo } from 'model/me'
+import { useQuery } from 'react-query';
+
+interface Response {
+  message: string;
+}
+
+function getMyPage() {
+  return http.get<MyInfo>('/users/mypage');
+}
+
+function editMyPage(data: MyInfo) {
+  return http.put<Response, MyInfo>('/users/mypage', data);
+}
+
+function postWithdrawal() {
+  // body에 필요한 것 있지 않을지..
+  return http.post<Response, undefined>('/users/withdrawal');
+}
+
+export function useGetMyPage() {
+  return useQuery('getMyPage', getMyPage);
+}
+
+export function useEditMyPage(data: MyInfo) {
+  return useQuery('putMyPage', () => editMyPage(data));
+}
+
+export function usePostWithdrawal() {
+  return useQuery('postWithdrawal', postWithdrawal);
+}
