358fa80fe4954e270aa7f2c5dc07f88e18c51d3a
MiaJLee <angella555@naver.com>
2021-06-10 01:54:38 +0900

    fix: datepicker pc view css

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -7,17 +7,20 @@ import { changeDateToString, makeDateFormat } from 'utils';
 
 import { mediaQuery, pxToVw } from 'styles/media';
 import { Body1, Heading7 } from 'styles/typography';
-import { flexCenter } from 'styles/containers';
+import { flexCenter, flexAlignCenter } from 'styles/containers';
 import { Close } from 'styles/icon';
 import color from 'styles/colors';
 import '../../node_modules/react-datepicker/dist/react-datepicker.css';
 import './date-picker.scss';
 import Button from './button';
 
-const datePickerWrap = css`
-  display: flex;
-  align-self: flex-start;
+const ShowPickerButton = styled(Body1)`
+  cursor: pointer;
+`;
+const Wrap = styled.div`
+  ${flexAlignCenter};
   margin-top: ${pxToVw(16)};
+  position: relative;
 
   ${mediaQuery(640)} {
     margin-top: 16px;
@@ -32,18 +35,36 @@ const customInput = css`
   font-size: 28px;
 `;
 
-const DatePickerWrap = styled.div`
+const DatePickerWrap = styled.div<{ isEndDate: boolean }>`
   position: fixed;
   top: 0;
   left: 0;
   z-index: 10;
-  background-color: ${color.grayscale.gray07};
   min-height: 100vh;
+  background-color: ${color.grayscale.gray07};
+
+  ${mediaQuery(640)} {
+    position: absolute;
+    top: 50px;
+    width: 375px;
+    min-height: auto;
+    box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.16);
+    border-radius: 16px;
+    height: ${({ isEndDate }) => (isEndDate ? 615 : 530)}px;
+  }
 `;
 
 const ButtonWrap = styled.div`
   width: 87.2vw;
   margin: 0 auto;
+
+  ${mediaQuery(640)} {
+    position: absolute;
+    width: 327px;
+    min-height: auto;
+    bottom: 32px;
+    left: 24px;
+  }
 `;
 
 const Label = styled(Heading7)`
@@ -55,6 +76,11 @@ const Header = styled.div`
   top: 20px;
   z-index: 11;
   left: calc(50vw - 45px);
+
+  ${mediaQuery(640)} {
+    top: 20px;
+    left: 142.5px;
+  }
 `;
 
 const CloseButton = styled.div`
@@ -65,6 +91,10 @@ const CloseButton = styled.div`
   height: 44px;
   ${flexCenter};
   z-index: 11;
+
+  ${mediaQuery(640)} {
+    top: 10px;
+  }
 `;
 
 registerLocale('ko', ko);
@@ -106,12 +136,12 @@ export default function DateRangeSelector({ setDate }: DatePickerProps) {
   };
 
   return (
-    <div css={datePickerWrap}>
-      <Body1 onClick={() => setShowPicker(!showPicker)} onKeyDown={() => setShowPicker(!showPicker)}>
+    <Wrap>
+      <ShowPickerButton onClick={() => setShowPicker(!showPicker)} onKeyDown={() => setShowPicker(!showPicker)}>
         {endDate ? `${makeDateFormat(startDate)} - ${makeDateFormat(endDate)}` : '여행일정 선택'}
-      </Body1>
+      </ShowPickerButton>
       {showPicker && (
-        <DatePickerWrap>
+        <DatePickerWrap isEndDate={!!endDate}>
           <Header>
             <Heading7>여행일정 선택</Heading7>
           </Header>
@@ -139,6 +169,6 @@ export default function DateRangeSelector({ setDate }: DatePickerProps) {
           )}
         </DatePickerWrap>
       )}
-    </div>
+    </Wrap>
   );
 }
