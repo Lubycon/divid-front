eb09fb6e928ef6cc2d8e9a02b4c6199b6aeb30ee
MiaJLee <angella555@naver.com>
2021-06-20 02:49:30 +0900

    add: useTooltip, tooltip components

--- src/pages/project-list/index.tsx
+++ src/pages/project-list/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import { basicWrap } from 'styles/containers';
+import React, { useEffect } from 'react';
+import { getLocalStorage } from 'utils';
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { mediaQuery, pxToVw } from 'styles/media';
+import { basicWrap } from 'styles/containers';
 import { Heading4 } from 'styles/typography';
 import { useGetTripLists } from 'hooks/data/useTripInfo';
+import useTooltip from 'hooks/useTooltip';
+import { Tooltip } from 'components/modal/tooltip';
 import Loading from 'pages/loading';
 import Empty from './empty';
 import Card from './card';
@@ -17,7 +22,29 @@ export default function ProjectList() {
   const currentTrip = data?.filter((trip) => !trip.end);
   const pastTrip = data?.filter((trip) => trip.end);
 
-  console.log(data);
+  const { handleOpen: openCreateTooltip, renderModal: renderCreateTooltip } = useTooltip({
+    children: (
+      <Tooltip
+        position={css`
+          top: ${pxToVw(65)};
+          right: ${pxToVw(24)};
+          ${mediaQuery(640)} {
+            top: 70px;
+            right: 25px;
+          }
+        `}
+        text="버튼을 눌러 여행을 만들어보세요!"
+      />
+    ),
+    type: 'create-trip'
+  });
+
+  useEffect(() => {
+    if (!getLocalStorage('create-trip') && !isLoading) {
+      openCreateTooltip();
+    }
+    openCreateTooltip();
+  }, [isLoading]);
 
   if (isLoading) {
     <Loading />;
@@ -33,42 +60,45 @@ export default function ProjectList() {
   }
 
   return (
-    <div css={basicWrap}>
-      <Welcome />
-      {currentTrip && currentTrip.length > 0 ? (
-        <>
-          <Title>여행 중</Title>
-          {currentTrip.map(({ tripId, tripName, startDate, endDate, memberCnt, userInfoResponseList, end }) => (
-            <Card
-              key={tripId}
-              tripId={tripId}
-              tripName={tripName}
-              startDate={startDate}
-              endDate={endDate}
-              memberCnt={memberCnt}
-              members={userInfoResponseList}
-              isCurrent={!end}
-            />
-          ))}
-        </>
-      ) : null}
-      {pastTrip && pastTrip.length > 0 ? (
-        <>
-          <Title>모든 여행</Title>
-          {pastTrip.map(({ tripId, tripName, startDate, endDate, memberCnt, userInfoResponseList, end }) => (
-            <Card
-              key={tripId}
-              tripId={tripId}
-              tripName={tripName}
-              startDate={startDate}
-              endDate={endDate}
-              memberCnt={memberCnt}
-              members={userInfoResponseList}
-              isCurrent={!end}
-            />
-          ))}
-        </>
-      ) : null}
-    </div>
+    <>
+      {renderCreateTooltip()}
+      <div css={basicWrap}>
+        <Welcome />
+        {currentTrip && currentTrip.length > 0 ? (
+          <>
+            <Title>여행 중</Title>
+            {currentTrip.map(({ tripId, tripName, startDate, endDate, memberCnt, userInfoResponseList, end }) => (
+              <Card
+                key={tripId}
+                tripId={tripId}
+                tripName={tripName}
+                startDate={startDate}
+                endDate={endDate}
+                memberCnt={memberCnt}
+                members={userInfoResponseList}
+                isCurrent={!end}
+              />
+            ))}
+          </>
+        ) : null}
+        {pastTrip && pastTrip.length > 0 ? (
+          <>
+            <Title>모든 여행</Title>
+            {pastTrip.map(({ tripId, tripName, startDate, endDate, memberCnt, userInfoResponseList, end }) => (
+              <Card
+                key={tripId}
+                tripId={tripId}
+                tripName={tripName}
+                startDate={startDate}
+                endDate={endDate}
+                memberCnt={memberCnt}
+                members={userInfoResponseList}
+                isCurrent={!end}
+              />
+            ))}
+          </>
+        ) : null}
+      </div>
+    </>
   );
 }
