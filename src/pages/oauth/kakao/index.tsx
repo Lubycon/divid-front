ed8be271e86bbe5876e26e625deaeda4c6f733d5
MiaJLee <angella555@naver.com>
2021-09-27 23:23:02 +0900

    fix: 카카오로그인 원복

---
+++ src/pages/oauth/kakao/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { setLocalStorage, useQueryString } from 'utils';
+import useKakaoToken from 'hooks/data/useKakaoToken';
+import Loading from 'pages/loading';
+
+export default function KakaoLogin() {
+  const code = useQueryString().get('code');
+  const state = useQueryString().get('state');
+  const { isLoading, data } = useKakaoToken(code || '');
+  const history = useHistory();
+
+  if (!isLoading && data) {
+    const { accessToken, refreshToken } = data;
+    setLocalStorage('accessToken', accessToken);
+    setLocalStorage('refreshToken', refreshToken);
+
+    if (state) {
+      accessToken && history.push(`/trips?tripId=${state}`);
+      return <Loading />;
+    }
+
+    accessToken && history.push('/projects');
+  }
+
+  return <Loading />;
+}
